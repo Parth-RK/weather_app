d89896cc2f132da5237f8780ff090205ce626535
Parth-RK <2021bcs119@sggs.ac.in>
2024-10-27 04:44:20 +0530

    added stock-market variables & placeholders

---
+++ static/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+        const weatherContent = document.getElementById("weather-content");
+        const loading = document.getElementById("loading-weather");
+
+        if (weatherContent && loading) {
+            // Hide loading and display weather data
+            // loading.style.display = "none";
+            // weatherContent.style.display = "block";
+        }
+    });
+
